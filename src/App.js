d2d0fe7f08b5c8b30ea14a00941a7be1d1881b6f
Szan71 <khadkasujan7@gmail.com>
2021-11-26 17:17:09 +0545

    Third commit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/mywork" element={<MyWork />} />
+          <Route path="/mywork" element={<MyWork />}  />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
