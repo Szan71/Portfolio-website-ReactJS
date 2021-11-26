918c21af61bf2f8b197793f618357aa2d88b1613
Szan71 <khadkasujan7@gmail.com>
2021-11-26 17:13:33 +0545

    Second commit

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/mywork" element={<MyWork />} />
-          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects" element={<Projects />}  />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
